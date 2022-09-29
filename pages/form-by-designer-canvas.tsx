487e98595c41fa29dbde72d76954d1f1eaa9d146
rainforss <shengyan@ualberta.ca>
2022-09-29 09:23:48 -0600

    added tracking

--- pages/form-by-designer-canvas.tsx
+++ pages/form-by-designer-canvas.tsx
@@ -13,15 +13,31 @@ const FormByDesignerCanvas: React.FunctionComponent<
       w="100%"
       bg="linear-gradient(to top right, #548D9A 0%, #404040 100%)"
     >
-      <Box w="30%" h="80vh" bg="white" borderRadius="10px" p="2rem">
-        <div data-form-block-id="a1f005e6-b030-ed11-9db0-000d3af4fa76"></div>
+      <Box
+        w="30%"
+        h="80vh"
+        bg="white"
+        borderRadius="10px"
+        p="2rem"
+        dangerouslySetInnerHTML={{
+          __html: `<div data-form-block-id="a1f005e6-b030-ed11-9db0-000d3af4fa76"></div>
+        <div id="dKNNozqqrFm67u-5ar8Nb5AnkgQJQztxqb5LA8wbVhes"></div>
+        <div
+          class="d365-mkt-config"
+          style={{ display: "none" }}
+          data-website-id="KNNozqqrFm67u-5ar8Nb5AnkgQJQztxqb5LA8wbVhes"
+          data-hostname="92164b15708a49468ceb8389bcf7d220.svc.dynamics.com"
+        ></div>`,
+        }}
+      >
+        {/* <div data-form-block-id="a1f005e6-b030-ed11-9db0-000d3af4fa76"></div>
         <div id="dKNNozqqrFm67u-5ar8Nb5AnkgQJQztxqb5LA8wbVhes"></div>
         <div
           className="d365-mkt-config"
           style={{ display: "none" }}
           data-website-id="KNNozqqrFm67u-5ar8Nb5AnkgQJQztxqb5LA8wbVhes"
           data-hostname="92164b15708a49468ceb8389bcf7d220.svc.dynamics.com"
-        ></div>
+        ></div> */}
       </Box>
     </Center>
   );
