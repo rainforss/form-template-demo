1402d8c752b5910aeb8990e93f2923bc5fd61725
rainforss <shengyan@ualberta.ca>
2022-10-18 18:48:13 -0600

    added real-time demo

--- forms/PurchaseForm.tsx
+++ forms/PurchaseForm.tsx
@@ -6,53 +6,42 @@ import CustomNumberInput from "../components/CustomNumberInput";
 interface IPurchaseFormProps {}
 
 interface PurchaseInfoValue {
-  amount: number;
+  amount: string;
 }
 
 const PurchaseForm: React.FunctionComponent<IPurchaseFormProps> = (props) => {
-  const toast = useToast();
   const [totalAmount, setTotalAmount] = React.useState(0);
-  const handleTabClosing = () => {
-    track_msdynmkt_test;
-  };
 
-  const track_msdynmkt_test = () => {
-    (window["msdynmkt" as any] as any).setUser({
+  const track_msdynmkt_test = async (amount: number) => {
+    await (window["msdynmkt" as any] as any).setUser({
       authId: "rainforss123@gmail.com",
     }); // ID, e-mail or phone number - see instructions
-    (window["msdynmkt" as any] as any).trackEvent({
+    await (window["msdynmkt" as any] as any).trackEvent({
       name: "msdynmkt_test", //Trigger title: Test
       ingestionKey:
         "03f9833e415547d1ba1927136e50c8e7-2c6b926b-6cad-40bc-8deb-2741510efb06-7059",
       version: "1.0.0",
       // To learn more about the event properties below, please see the documentation on  To learn more about the event properties below, please see the documentation on for custom triggers.
       properties: {
-        amount: totalAmount,
-        bindingid: `test/${totalAmount}`,
+        amount: amount,
+        bindingid: `test/${amount}`,
       },
     });
   };
 
-  const alertUser = (event: any) => {
-    event.preventDefault();
-    event.returnValue = "Test";
-  };
-  React.useEffect(() => {
-    window.addEventListener("beforeunload", alertUser);
-    window.addEventListener("unload", handleTabClosing);
-    return () => {
-      window.removeEventListener("beforeunload", alertUser);
-      window.removeEventListener("unload", handleTabClosing);
-    };
-  });
   return (
     <Formik
-      initialValues={{ amount: totalAmount }}
-      onSubmit={(values, actions) => {
+      initialValues={{ amount: totalAmount.toString() }}
+      onSubmit={async (values, actions) => {
         actions.setSubmitting(true);
-        setTotalAmount(() => values.amount);
+        setTotalAmount(() => parseInt(values.amount));
         alert(`Your purchasing amount is ${values.amount}`);
         actions.setSubmitting(false);
+      }}
+      onReset={async (values, actions) => {
+        await track_msdynmkt_test(parseInt(values.amount));
+        actions.setValues({ amount: "0" });
+        setTotalAmount(() => 0);
       }}
     >
       {(props: FormikProps<PurchaseInfoValue>) => {
@@ -78,6 +67,17 @@ const PurchaseForm: React.FunctionComponent<IPurchaseFormProps> = (props) => {
               disabled={props.isSubmitting}
             >
               Update
+            </Button>
+            <Button
+              type="reset"
+              mr="auto"
+              my="2rem"
+              bgColor="#173f5e"
+              color="white"
+              px="2rem"
+              py="1.5rem"
+            >
+              Reset
             </Button>
           </Form>
         );
