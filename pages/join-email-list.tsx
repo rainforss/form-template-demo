51040f8055ff7977230b90f36caadace57dd4b03
unknown <shengyan@ualberta.ca>
2023-10-03 12:52:32 -0600

    adjusted form container height

--- pages/join-email-list.tsx
+++ pages/join-email-list.tsx
@@ -6,11 +6,10 @@ interface IDemoFormProps {}
 const DemoForm: React.FunctionComponent<IDemoFormProps> = () => {
   return (
     <Center
-      h="100vh"
       w="100%"
       bg="linear-gradient(to top right, #548D9A 0%, #404040 100%)"
     >
-      <Box w="30%" h="80vh" bg="white" borderRadius="10px" p="2rem">
+      <Box w="30%" h="fit-content" bg="white" borderRadius="10px" p="2rem">
         <div data-form-block-id="882ef3ec-1c62-ee11-be6e-00224825b9dd"></div>
         <div id="dkWcCr2cBRe1rwuY37vTJuSbgiPiZp4_SQBuhApKiLt4"></div>
         <div
