b2ce92cab242dfa1346b772403aa9e7e89ea4b48
unknown <shengyan@ualberta.ca>
2023-10-03 12:46:32 -0600

    added subscription form

---
+++ pages/join-email-list.tsx
@@ -0,0 +1,35 @@
+import { Center, Box } from "@chakra-ui/react";
+import * as React from "react";
+
+interface IDemoFormProps {}
+
+const DemoForm: React.FunctionComponent<IDemoFormProps> = () => {
+  return (
+    <Center
+      h="100vh"
+      w="100%"
+      bg="linear-gradient(to top right, #548D9A 0%, #404040 100%)"
+    >
+      <Box w="30%" h="80vh" bg="white" borderRadius="10px" p="2rem">
+        <div data-form-block-id="882ef3ec-1c62-ee11-be6e-00224825b9dd"></div>
+        <div id="dkWcCr2cBRe1rwuY37vTJuSbgiPiZp4_SQBuhApKiLt4"></div>
+        <div
+          className="d365-mkt-config"
+          style={{ display: "none" }}
+          data-website-id="kWcCr2cBRe1rwuY37vTJuSbgiPiZp4_SQBuhApKiLt4"
+          data-hostname="78259aeb4bd244c0b3adb9f8257c48de.svc.dynamics.com"
+        ></div>
+      </Box>
+    </Center>
+  );
+};
+
+export const getServerSideProps = async () => {
+  return {
+    props: {
+      test: true,
+    },
+  };
+};
+
+export default DemoForm;
