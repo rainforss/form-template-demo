f372d38d15e026204ecf8ab87fe7c443a2663ec0
unknown <shengyan@ualberta.ca>
2023-10-04 14:39:11 -0600

    added google font

--- pages/subscription-center.tsx
+++ pages/subscription-center.tsx
@@ -9,7 +9,7 @@ const DemoForm: React.FunctionComponent<IDemoFormProps> = () => {
       w="100%"
       bg="linear-gradient(to top right, #548D9A 0%, #404040 100%)"
     >
-      <Box w="60%" h="fit-content" bg="white" borderRadius="10px" p="2rem">
+      <Box w="60%" h="fit-content" bg="#efeff4" borderRadius="10px" p="2rem">
         <div data-form-block-id="7fe71819-ea62-ee11-be6e-00224825b9dd"></div>
         <div id="dkWcCr2cBRe1rwuY37vTJuSbgiPiZp4_SQBuhApKiLt4"></div>
         <div
