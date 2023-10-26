160b14d209f0b88e185d726fbf313394bc50ee6e
unknown <shengyan@ualberta.ca>
2023-10-26 12:41:57 -0600

    RTM form POC

--- pages/request-a-quote.tsx
+++ pages/request-a-quote.tsx
@@ -4,7 +4,24 @@ import ContactInfoForm from "../forms/ContactInfoForm";
 
 interface IDemoFormProps {}
 
+declare global {
+  interface Window {
+    d365mktforms: any;
+  }
+}
+
 const DemoForm: React.FunctionComponent<IDemoFormProps> = () => {
+  React.useEffect(() => {
+    document
+      .getElementById("d365-marketing-form-placeholder")
+      ?.appendChild(
+        window.d365mktforms.createForm(
+          "d8fd5d51-cf6e-ee11-8df0-00224825b19c",
+          "https://public-usa.mkt.dynamics.com/api/v1.0/orgs/5dd0fa48-823b-ee11-94d2-000d3a106937/landingpageforms",
+          "https://assets-usa.mkt.dynamics.com/5dd0fa48-823b-ee11-94d2-000d3a106937/digitalassets/forms/d8fd5d51-cf6e-ee11-8df0-00224825b19c"
+        )
+      );
+  }, []);
   return (
     <Center
       w="100%"
@@ -19,13 +36,7 @@ const DemoForm: React.FunctionComponent<IDemoFormProps> = () => {
       >
         <ContactInfoForm />
       </Box>
-      <Box>
-        <div
-          data-form-id="d8fd5d51-cf6e-ee11-8df0-00224825b19c"
-          data-form-api-url="https://public-usa.mkt.dynamics.com/api/v1.0/orgs/5dd0fa48-823b-ee11-94d2-000d3a106937/landingpageforms"
-          data-cached-form-url="https://assets-usa.mkt.dynamics.com/5dd0fa48-823b-ee11-94d2-000d3a106937/digitalassets/forms/d8fd5d51-cf6e-ee11-8df0-00224825b19c"
-        ></div>
-      </Box>
+      <Box id="d365-marketing-form-placeholder"></Box>
     </Center>
   );
 };
