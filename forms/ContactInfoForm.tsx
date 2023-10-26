86865bbdecfef679f9a21c4cf4326f748fea072e
unknown <shengyan@ualberta.ca>
2023-10-26 14:30:11 -0600

    RTM form POC

--- forms/ContactInfoForm.tsx
+++ forms/ContactInfoForm.tsx
@@ -64,6 +64,20 @@ export class ContactInfoValue {
 }
 
 const ContactInfoForm: React.FunctionComponent<IContactInfoFormProps> = () => {
+  let d365FormRes: any;
+  React.useEffect(() => {
+    document
+      .querySelector("form.marketingForm")
+      ?.addEventListener("d365mkt_afterformsubmit", (e: any) => {
+        console.log(e.detail);
+        d365FormRes = e.detail.successful;
+      });
+    document
+      .querySelector("form.marketingForm")
+      ?.addEventListener("d365mkt_formsubmit", (e: any) => {
+        console.log(e.detail);
+      });
+  }, []);
   const submit = async (values: ContactInfoValue) => {
     console.log(values);
   };
@@ -72,7 +86,6 @@ const ContactInfoForm: React.FunctionComponent<IContactInfoFormProps> = () => {
     <Formik
       initialValues={new ContactInfoValue()}
       onSubmit={async (values, actions) => {
-        let d365FormRes;
         actions.setSubmitting(true);
         toast({
           status: "warning",
@@ -82,17 +95,6 @@ const ContactInfoForm: React.FunctionComponent<IContactInfoFormProps> = () => {
           isClosable: true,
           duration: 3000,
         });
-        document
-          .querySelector("form.marketingForm")
-          ?.addEventListener("d365mkt_afterformsubmit", (e: any) => {
-            console.log(e.detail);
-            d365FormRes = e.detail.successful;
-          });
-        document
-          .querySelector("form.marketingForm")
-          ?.addEventListener("d365mkt_formsubmit", (e: any) => {
-            console.log(e.detail);
-          });
 
         (
           document.querySelector(
